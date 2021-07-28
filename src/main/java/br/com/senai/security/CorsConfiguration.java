50144001f61716aadadd37e069512af2098ad93d
Brayan <brayan.11fera@gmail.com>
2021-07-28 14:14:40 -0300

    Apenas

--- src/main/java/br/com/senai/security/CorsConfiguration.java
+++ src/main/java/br/com/senai/security/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").
-        allowedOrigins("http://locahost:3000").
+        allowedOrigins("http://localhost:3000").
         allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 
