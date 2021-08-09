8c4abf8fad6df0609950d0505c814eb9bdfefd9b
Brayan <brayan.11fera@gmail.com>
2021-08-09 09:58:38 -0300

    Alterações para permitir o acesso do cors do navegador

--- src/main/java/br/com/senai/security/CorsConfiguration.java
+++ src/main/java/br/com/senai/security/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").
-        allowedOrigins("*").
+        allowedOrigins("http://localhost:3000").
         allowedMethods("*");
     }
 
